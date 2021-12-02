f5082c1305bab1a050239747fa46eab3b62242d1
Gerald <geraldnyeo@gmail.com>
2021-12-02 21:21:35 +0800

    Part of the basic UI
    Home Page UI
    Only the play button is working :(
    Level Select UI

---
+++ Assets/HomeEventSystem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HomeEventSystem : MonoBehaviour
+{
+    public Button playBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = playBtn.GetComponent<Button>();
+        btn.onClick.AddListener(delegate{
+            Redirect();
+        });
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            SceneManager.LoadScene("LevelSelect");
+        }
+    }
+
+    void Redirect() 
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+}
