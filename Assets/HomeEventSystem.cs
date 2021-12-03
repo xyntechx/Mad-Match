ef2b3bcd890ed296c0b3861328ff58919c4684a9
Gerald <geraldnyeo@gmail.com>
2021-12-03 11:24:22 +0800

    A little bit more UI stuff
    All buttons on Level Select page now work. (Do not transmit data about what level was selected yet.)
    (I'm just going to make the code work for now, but I'm not familiar with the best practices for unity code so it may be quite messy and inefficient, sorry.)
    (I never know what to put for these things, especially since I haven't really changed much, I just want to make sure everything on the site is up to date, in case somebody is editing.)

--- Assets/HomeEventSystem.cs
+++ Assets/HomeEventSystem.cs
@@ -21,9 +21,7 @@ public class HomeEventSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            SceneManager.LoadScene("LevelSelect");
-        }
+        
     }
 
     void Redirect() 
