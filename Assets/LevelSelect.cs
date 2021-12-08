77dddb4e48ae44d680d2083b45fdc619ca74a650
xyntechx <xyntechx@gmail.com>
2021-12-08 22:17:08 +0800

    🔖 Working scenes + rough game sample

---
+++ Assets/LevelSelect.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public Button button;
+
+    void Start()
+    {
+        button.GetComponent<Button>().onClick.AddListener(Navigate);
+    }
+
+    void Navigate()
+    {
+        switch (this.name)
+        {
+            case "Lvl1Btn":
+                SceneManager.LoadSceneAsync(4);
+                break;
+            case "Lvl2Btn":
+                SceneManager.LoadSceneAsync(5);
+                break;
+            case "Lvl3Btn":
+                SceneManager.LoadSceneAsync(6);
+                break;
+            case "Lvl4Btn":
+                SceneManager.LoadSceneAsync(7);
+                break;
+            case "Lvl5Btn":
+                SceneManager.LoadSceneAsync(8);
+                break;
+            default:
+                break;
+        }
+    }
+}
