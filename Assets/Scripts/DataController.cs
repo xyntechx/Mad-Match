7eac439f148b3941a3d8078c432d9078cc2fac55
Gerald <geraldnyeo@gmail.com>
2021-12-23 12:01:14 +0800

    Leaderboard
    Leaderboard
     - Added the podium image to the leaderboard screen.
     - Made scores update based on fake data (no request to server yet).

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -7,6 +7,8 @@ public static class DataController
     /*
     Controls data between scenes
         - Which level is loaded
+
+    Expanding this class to include the Leaderboard API
     */
 
     public static int LevelSelected {
